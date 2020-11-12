f7af7217e8ebca8bdfd6ae84135e8d962168992c
wxh <wxh170607@163.com>
2020-11-12 20:06:33 +0800

    first update

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # autoSelfreport
+
+上海大学新冠疫情自动填报脚本
+
+## 参考&鸣谢
+
+[rllll/selfreport 上海大学SHU自动每日一报](https://github.com/rllll/selfreport)
+
+## 代码介绍：
+`selfreport-win.py`是适用于windows操作系统的脚本，可以看到操作流程。
+
+`selfreport-linux.py`是适用于linux操作系统的服务器脚本，不会打开网页。
+
+## 使用方式：
+考虑到使用人信息，存放用户名和密码的`userInfo.json`不会提交上传，使用前请自行创建。格式如下：
+
+```
+# userInfo.json
+{
+    "userList":[{
+        "username": "YourUsername",
+        "password": "YourPassword"
+    }]
+}
+```
+创建并填写正确的账户和密码，运行操作系统对应的脚本即可。
+
+## TODO:
+1. 定时填报
+2. 代码封装
---
+++ selfreport-win.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+# coding=UTF-8
+from selenium import webdriver
+import time
+import random
+from selenium.webdriver.chrome.options import Options
+import json
+
+class SelfReport(object):
+
+    def __init__(self):
+        # chrome_options = Options()
+        # chrome_options.add_argument('--no-sandbox')
+        # chrome_options.add_argument('--disable-dev-shm-usage')
+        # chrome_options.add_argument('--headless')
+        # self.driver = webdriver.Chrome(executable_path=r'./chromedriver', chrome_options=chrome_options)
+        self.driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
+        with open('userInfo.json', mode="r", encoding="utf-8") as userFile:
+            self.userInfo = json.load(userFile)
+        self.username = self.userInfo["userList"][0]["username"]
+        self.password = self.userInfo["userList"][0]["password"]
+
+    def auto_report(self):
+        file_handle = open('log.txt', mode='a',encoding='utf-8')
+        driver = self.driver
+        driver.get('https://selfreport.shu.edu.cn/Default.aspx')
+        print("="*100)
+        file_handle.write('\n')
+        file_handle.write('='*100)
+        file_handle.write('\n')
+        print("已进入填报网站")
+        file_handle.write(time.ctime())
+        file_handle.write('\n')
+        username = driver.find_element_by_id("username")
+        username.send_keys(self.username)
+        password = driver.find_element_by_id("password")
+        password.send_keys(self.password)
+        print("自动填入账号密码完成")
+        submit = driver.find_element_by_id("submit")
+        submit.click()
+        print("进入每日一报网站")
+        
+        driver.find_element_by_id("lnkReport").click()
+        time.sleep(1)
+
+        driver.find_element_by_id("p1_Button2").click()
+        time.sleep(1)
+        
+        promise = driver.find_element_by_id("p1_ChengNuo-inputEl-icon")
+        promise.click()
+        print("勾选承诺完成")
+        time.sleep(0.5)
+
+        temperature = driver.find_element_by_id("p1_TiWen-inputEl")
+        store = ["36.8","36.9","37","37.1","37.2"]
+        i = random.randint(0,4)
+        choose = store[i]
+        temperature.send_keys(choose)
+        print("填报体温完成")
+        time.sleep(0.5)
+
+        healthCode = driver.find_element_by_id("fineui_7-inputEl-icon")
+        healthCode.click()
+        print("勾选绿色随申码")
+        time.sleep(0.5)
+#         可选项 不填写
+#         meal = driver.find_element_by_id("fineui_9-inputEl-icon")
+#         meal.click()
+#         print("勾选午餐")
+#         time.sleep(1)
+        
+        submit_res = driver.find_element_by_id("p1_ctl00_btnSubmit")
+        submit_res.click()
+        time.sleep(2)
+
+#         driver.find_elements_by_css_selector(".f-btn.f-noselect.f-state-default.f-corner-all"
+#                                                     ".f-btn-normal.f-btn-icon-no.f-cmp.f-widget"
+#                                                     ".f-toolbar-item")[3].click()
+        driver.find_element_by_id("fineui_14").click()
+        print(time.ctime())
+        time.sleep(5)
+        print("成功提交")
+
+        driver.close()
+        print("每日一报已完成")
+        print("="*100)
+        file_handle.write('体温为'+choose+'度\n')
+        file_handle.write('已完成每日一报自动填写\n')
+        file_handle.write('='*100)
+        file_handle.write('\n')
+
+        file_handle.close()
+
+    def run(self):
+        self.auto_report()
+
+
+if __name__ == '__main__':
+    sp = SelfReport()
+    sp.run()
