d4ab4fc42bb3e13ca72f7354a375795df0ef837e
寥落醒999 <962655354@qq.com>
2020-12-24 23:34:13 +0800

    update Timing(Random pattern)

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import Timing
+from Timing import timing
 import selfreport
 
 if __name__ == '__main__':
+    print("START")
+    start = ["7:05", "20:05"]
+    end = ["8:55", "21:55"]
     sp = selfreport.SelfReport()
+    userList = sp.readUserGroupInfo()
+    t = timing(userList)
+    t.setEffectTime(start,end)
+    t.setInterval()
     while True:
-        Timing.checktime(sp.run,sp.run)
+        t.checkTime(sp.run,5)
